795bdc1c972361b148cf50e71827d81f6ff0c3f6
mathilda0902 <mathilda_sorciere@yahoo.com>
2025-06-25 19:28:19 -0700

    feat: add ci and precommit

--- Package.swift
+++ Package.swift
@@ -10,6 +10,7 @@ let package = Package(
         // Targets can depend on other targets in this package and products from dependencies.
         .executableTarget(
             name: "waypoint-stories-backend",
-            path: "Sources"),
+            path: "Sources"
+        ),
     ]
 )
